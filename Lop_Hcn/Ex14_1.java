6649162f054553f3faa26b1c2e91effa7eb849fa
NguyenQuang99 <quanghero7@gmail.com>
2020-09-14 10:53:47 +0700

    Lop_Hcn/

---
+++ Lop_Hcn/Ex14_1.java
@@ -0,0 +1,11 @@
+package lop_Hcn;
+
+public class Ex14_1 {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(4.0,5.0);
+        System.out.println(rectangle.display());
+        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+    }
+
+}
